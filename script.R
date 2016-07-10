9b0e6675e1ffa53e58f89d7fcbe7fa1019775a1b
nraybaker <nraybaker@gmail.com>
2016-07-10 18:16:53 -0500

    Update script.R

--- script.R
+++ script.R
@@ -1,4 +1,12 @@
 rm(list = ls())
+
+Mode <- function(x){
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+t <- proc.time()
+options(scipen = 999)
 require(nnet)
 require(dplyr)
 require(tidyr)
@@ -12,15 +20,18 @@ lst <- lapply(test$Sequence, strsplit, ",")
 lst <- sapply(lst, sapply, as.numeric)
 
 
-
-
+# Finding Independents
 df_preds_indep <- data.frame("Id" = test$Id)
 df_preds_indep$Last <- NA
 df_preds_indep$Independent <- FALSE
-# Finding Independents
-for(k in 26354:length(lst)){
+
+for(k in 1:length(lst)){
   while(length(lst[[k]]) < 2){
     df_preds_indep$Last[k] <- 0
+    k <- k + 1
+  }
+  
+  if(k == 26353){
     k <- k + 1
   }
   
@@ -32,7 +43,7 @@ for(k in 26354:length(lst)){
     }
     x <- x + 1
   }
-  nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 2), ], size = 1, linout = TRUE)
+  nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 2), ], size = 1, linout = TRUE, skip = TRUE)
   df_preds_indep$Last[k] <- round(predict(nn, df[(nrow(df) - 1), ]), 0)
   
   if (df_preds_indep$Last[k] == df[nrow(df), 1]){
@@ -41,6 +52,7 @@ for(k in 26354:length(lst)){
   
   print(k)
 }  
+save.image("~/R/SequenceKaggle/step1.RData")
 
 
 # Finding 1 Recursives
@@ -48,9 +60,13 @@ df_preds_one_cursive <- data.frame("Id" = test$Id)
 df_preds_one_cursive$Last <- NA
 df_preds_one_cursive$OneCursive <- FALSE
 
-for(k in 78169:length(lst)){
+for(k in 1:length(lst)){
   while(length(lst[[k]]) < 3){
     df_preds_one_cursive$Last[k] <- 0
+    k <- k + 1
+  }
+  
+  if((k == 26353) | (k == 78168)){
     k <- k + 1
   }
   
@@ -62,7 +78,7 @@ for(k in 78169:length(lst)){
     }
     x <- x + 1
   }
-  nn <- nnet(Test ~ One + Two, data = df[1:(nrow(df) - 2), ], size = 2, linout = TRUE)
+  nn <- nnet(Test ~ One + Two, data = df[1:(nrow(df) - 2), ], size = 2, linout = TRUE, skip = TRUE)
   df_preds_one_cursive$Last[k] <- round(predict(nn, df[(nrow(df) - 1), ]), 0)
   
   if (df_preds_one_cursive$Last[k] == df[nrow(df), 1]){
@@ -71,6 +87,8 @@ for(k in 78169:length(lst)){
   
   print(k)
 }  
+save.image("~/R/SequenceKaggle/step2.RData")
+
 
 # Finding 2 Recursives
 df_preds_two_cursive <- data.frame("Id" = test$Id)
@@ -95,7 +113,7 @@ for(k in 1:length(lst)){
     }
     x <- x + 1
   }
-  nn <- nnet(Test ~ One + Two + Three, data = df[1:(nrow(df) - 2), ], size = 3, linout = TRUE)
+  nn <- nnet(Test ~ One + Two + Three, data = df[1:(nrow(df) - 2), ], size = 3, linout = TRUE, skip = TRUE)
   df_preds_two_cursive$Last[k] <- round(predict(nn, df[(nrow(df) - 1), ]), 0)
   
   if (df_preds_two_cursive$Last[k] == df[nrow(df), 1]){
@@ -104,20 +122,57 @@ for(k in 1:length(lst)){
   
   print(k)
 }
+save.image("~/R/SequenceKaggle/step3.RData")
+
+# Finding 3 Recursives
+df_preds_three_cursive <- data.frame("Id" = test$Id)
+df_preds_three_cursive$Last <- NA
+df_preds_three_cursive$ThreeCursive <- FALSE
+
+for(k in 1:length(lst)){
+  while(length(lst[[k]]) < 5){
+    df_preds_three_cursive$Last[k] <- 0
+    k <- k + 1
+  }
+  
+  if(k == 26353){
+    k <- k + 1
+  }
+  
+  df <- data.frame("One" = 0, "Two" = 0, "Three" = 0, "Four" = 0, "Test" = 0)
+  x <- 1
+  for(i in 1:(length(lst[[k]]) - 3)){
+    for(j in 0:4){
+      df[x, j + 1] <- lst[[k]][i + j]
+    }
+    x <- x + 1
+  }
+  nn <- nnet(Test ~ One + Two + Three + Four, data = df[1:(nrow(df) - 2), ], size = 4, linout = TRUE, skip = TRUE)
+  df_preds_three_cursive$Last[k] <- round(predict(nn, df[(nrow(df) - 1), ]), 0)
+  
+  if (df_preds_three_cursive$Last[k] == df[nrow(df), 1]){
+    df_preds_three_cursive$ThreeCursive[k] <- TRUE
+  }
+  
+  print(k)
+}
+save.image("~/R/SequenceKaggle/step4.RData")
 
 
+
+t <- proc.time()
 # Finals
 df_preds <- data.frame("Id" = test$Id)
 df_preds$Last <- NA
 
 for(k in 1:length(lst)){
   while(length(lst[[k]]) < 2){
-    df_preds$Last[k] <- 0
+    df_preds$Last[k] <- 0 #Mode(lst[[k]])
     k <- k + 1
   }
   
-  if(k == 19911 | k == 26353){
-    df_preds$Last[k] <- 0
+  if((k == 19911) | (k == 26353) | (k == 78168)){
+    df_preds$Last[k] <- 0 #Mode(lst[[k]])
     k <- k + 1
   }
   
@@ -130,7 +185,7 @@ for(k in 1:length(lst)){
       }
       x <- x + 1
     }
-    nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 1), ], size = 1, linout = TRUE)
+    nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 1), ], size = 1, linout = TRUE, skip = TRUE)
     df_preds$Last[k] <- round(predict(nn, df[nrow(df), ]), 0)
   }
   
@@ -143,7 +198,7 @@ for(k in 1:length(lst)){
       }
       x <- x + 1
     }
-    nn <- nnet(Test ~ One + Two, data = df[1:(nrow(df) - 1), ], size = 2, linout = TRUE)
+    nn <- nnet(Test ~ One + Two, data = df[1:(nrow(df) - 1), ], size = 2, linout = TRUE, skip = TRUE)
     df_preds$Last[k] <- round(predict(nn, df[nrow(df), ]), 0)
   }
   
@@ -156,7 +211,20 @@ for(k in 1:length(lst)){
       }
       x <- x + 1
     }
-    nn <- nnet(Test ~ One + Two + Three, data = df[1:(nrow(df) - 1), ], size = 1, linout = TRUE)
+    nn <- nnet(Test ~ One + Two + Three, data = df[1:(nrow(df) - 1), ], size = 3, linout = TRUE, skip = TRUE)
+    df_preds$Last[k] <- round(predict(nn, df[nrow(df), ]), 0)
+  }
+  
+  else if(isTRUE(df_preds_three_cursive$ThreeCursive[k])){
+    df <- data.frame("One" = 0, "Two" = 0, "Three" = 0, "Four" = 0, "Test" = 0)
+    x <- 1
+    for(i in 1:(length(lst[[k]]) - 3)){
+      for(j in 0:4){
+        df[x, j + 1] <- lst[[k]][i + j]
+      }
+      x <- x + 1
+    }
+    nn <- nnet(Test ~ One + Two + Three + Four, data = df[1:(nrow(df) - 1), ], size = 4, linout = TRUE, skip = TRUE)
     df_preds$Last[k] <- round(predict(nn, df[nrow(df), ]), 0)
   }
   
@@ -169,15 +237,13 @@ for(k in 1:length(lst)){
       }
       x <- x + 1
     }
-    nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 1), ], size = 1, linout = TRUE)
+    nn <- nnet(Test ~ One, data = df[1:(nrow(df) - 1), ], size = 1, linout = TRUE, skip = TRUE)
     df_preds$Last[k] <- round(predict(nn, df[nrow(df), ]), 0)
   }
   
   print(k)
 }
+save.image("~/R/SequenceKaggle/step5.RData")
+proc.time() - t
 
-
-
-
-  
 write_csv(df_preds, "submission.csv")
